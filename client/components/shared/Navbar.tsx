016a041b242576b886cad8057eb2eb4a72dd0e32
aljithkj02 <kwdeathlok@gmail.com>
2024-07-24 11:02:51 +0530

    Add basic next app

---
+++ client/components/shared/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "@/assets"
+import Image from "next/image"
+
+export const Navbar = () => {
+  return (
+    <div className="bg-black">
+        <div>
+            <Image src={Logo} alt="logo" />
+        </div>
+
+        <div>
+            
+        </div>
+    </div>
+  )
+}
